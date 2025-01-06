ad7d1a6a3c73885503c9f450d89e63d303c0a33d
MarcoCSL <marco.cosulich019@gmail.com>
2025-01-06 19:09:17 +0100

    Aggiunto BUBBLE PLOT e fix BAR PLOT

--- Cosulich/graph.js
+++ Cosulich/graph.js
@@ -1,6 +1,10 @@
 const margin_11_15 = { top: 30, right: 30, bottom: 100, left: 100 },
 	width_11_15 = 550 - margin_11_15.left - margin_11_15.right,
 	height_11_15 = 550 - margin_11_15.top - margin_11_15.bottom;
+
+const bubble_margin = { top: 30, right: 30, bottom: 100, left: 100 },
+	bubble_width = 1250 - bubble_margin.left - bubble_margin.right,
+	bubble_height = 650 - bubble_margin.top - bubble_margin.bottom;
 	
 const colours_11_15 = [
 	"#4C6A92",
@@ -46,10 +50,10 @@ const svg_plot14 = d3
 const svg_plot15 = d3
 	.select("#plot15")
 	.append("svg")
-	.attr("width", width_11_15 + margin_11_15.left + margin_11_15.right)
-	.attr("height", height_11_15 + margin_11_15.top + margin_11_15.bottom)
+	.attr("width", bubble_width + bubble_margin.left + bubble_margin.right)
+	.attr("height", bubble_height + bubble_margin.top + bubble_margin.bottom)
 	.append("g")
-	.attr("transform", `translate(${margin_11_15.left},${margin_11_15.top})`);
+	.attr("transform", `translate(${bubble_margin.left},${bubble_margin.top})`);
 
 function add_axis_label(svg_plot, x, y, transform, text_anchor, label) {
 	svg_plot
@@ -115,19 +119,20 @@ function bar_plot(data, svg_plot, id_div) {
 		.attr("class", "tooltip")
 		.style("background-color", "white")
 		.style("border", "solid")
-		.style("border-width_11_15", "1px")
+		.style("border-width", "1px")
 		.style("border-radius", "5px")
 		.style("padding", "10px")
 		.style("position", "absolute")
 		.style("left", "0px")
-		.style("top", "0px");
+		.style("top", "0px")
+		.style("line-height", "1.4");
 
 	var mouseover = function (event, d) {
 		d3.selectAll(id_div + "  rect").style("opacity", 0.2);
 		d3.select(this).style("opacity", 1);
 		info = d3.select(this).datum();
 		tooltip
-			.html(info.production + " Tonnes of heavy metal (tHM)")
+			.html("Country: <b>" + info.country + "</b><br>Production: <b>" + info.production + "</b> Tonnes of heavy metal (tHM)")
 			.style("opacity", 1);
 	};
 
@@ -240,12 +245,13 @@ function stacked_bar_plot(data, svg_plot, id_div) {
 		.attr("class", "tooltip")
 		.style("background-color", "white")
 		.style("border", "solid")
-		.style("border-width_11_15", "1px")
+		.style("border-width", "1px")
 		.style("border-radius", "5px")
 		.style("padding", "10px")
 		.style("position", "absolute")
 		.style("left", "0px")
-		.style("top", "0px");
+		.style("top", "0px")
+		.style("line-height", "1.4");	
 
 	var mouseover = function (event, d) {
 						d3.selectAll(id_div + "  rect").style("opacity", 0.2);
@@ -295,6 +301,112 @@ function stacked_bar_plot(data, svg_plot, id_div) {
 		.on("mouseleave", mouseleave);
 }
 
+function bubbe_plot(data, svg_plot, id_div) {
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.GDP)])
+        .range([0, bubble_width]);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.taxes) * 1.1])
+        .range([bubble_height, 0]);
+
+    const radius = d3.scaleSqrt()
+        .domain([0, d3.max(data, d => d.investments)])
+        .range([1, 70]);
+
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
+
+    svg_plot.append("g")
+        .attr("transform", "translate(0," + bubble_height + ")")
+        .call(xAxis);
+  
+    svg_plot.append("g")
+        .call(yAxis);
+
+    svg_plot.append("text")
+        .attr("class", "axis-label")
+        .attr("x", bubble_width / 2)
+        .attr("y", bubble_height + 60)
+        .style("text-anchor", "middle")
+        .text("GDP");
+
+    svg_plot.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -bubble_height / 2)
+        .attr("y", -70)
+        .style("text-anchor", "middle")
+        .text("Environmental Tax (millions EUR)");
+
+    const tooltip = d3.select(id_div)
+				.append("div")
+				.style("opacity", 0)
+				.attr("class", "tooltip")
+				.style("background-color", "white")
+				.style("border", "solid")
+				.style("border-width", "1px")
+				.style("border-radius", "5px")
+				.style("padding", "10px")
+				.style("position", "absolute")
+				.style("left", "0px")
+				.style("top", "0px")
+				.style("line-height", "1.4");
+
+	const formatta = d3.format(".3~s");
+
+	const colours_27 = [
+		"#FF6347", "#FF4500", "#FFD700", "#ADFF2F", "#32CD32", "#3CB371", 
+		"#20B2AA", "#48D1CC", "#00BFFF", "#1E90FF", "#6495ED", "#8A2BE2", 
+		"#9932CC", "#D8BFD8", "#FF69B4", "#FF1493", "#C71585", "#800080", 
+		"#8B0000", "#DC143C", "#FF8C00", "#FF7F50", "#7FFF00", "#98FB98", 
+		"#C1FFC1", "#FFFF00", "#FFC0CB"
+	];
+
+	svg_plot.append("line")
+    .attr("x1", x(100))
+    .attr("x2", x(100))
+    .attr("y1", 0)
+    .attr("y2", bubble_height)
+    .attr("stroke", "red")
+    .attr("stroke-dasharray", "5,5")
+    .attr("stroke-width", 2);
+
+    const bubbles = svg_plot.selectAll(".bubble")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "bubble")
+        .attr("cx", d => x(d.GDP))
+        .attr("cy", d => y(d.taxes))
+        .attr("r", d => radius(d.investments))
+        .style("fill", (d, i) => colours_27[i % colours_27.length])
+        .style("opacity", 0.7)
+        .style("stroke", "black")
+        .style("stroke-width", 1)
+        .on("mouseover", function(event, d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html("Country: <b>" + d.country + "</b><br>Investments: <b>" + formatta(d.investments) + "</b> millions EUR<br>GDP: <b>" + d.GDP + "</b><br>Tax: <b>" + formatta(d.taxes) + "</b> millions EUR")
+                .style("left", (event.pageX + 30) + "px")
+                .style("top", (event.pageY - 130) + "px");
+        })
+        .on("mouseout", function(d) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
+}
+
+d3.csv("Cosulich/plot1.csv", function (d) {
+	return {
+		country: d.country,
+		production: +d.production
+	};
+}).then(function (data) {
+	bar_plot(data, svg_plot11, "#plot11");
+});
+
 d3.csv("Cosulich/plot2.csv", function (d) {
 	return {
 		country: d.country,
@@ -315,4 +427,24 @@ d3.csv("Cosulich/plot3.csv", function (d) {
 	};
 }).then(function (data) {
 	bar_plot(data, svg_plot13, "#plot13");
+});
+
+d3.csv("Cosulich/plot4.csv", function (d) {
+	return {
+		country: d.country,
+		taxes: +d.taxes
+	};
+}).then(function (data) {
+	bar_plot(data, svg_plot14, "#plot14");
+});
+
+d3.csv("Cosulich/plot5.csv", function (d) {
+	return {
+		country: d.country,
+		investments: +d.investments,
+		taxes: +d.taxes,
+		GDP: +d.GDP
+	};
+}).then(function (data) {
+	bubbe_plot(data, svg_plot15, "#plot15");
 });
